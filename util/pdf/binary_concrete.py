9caef3c34e7d9e5722958e9e94aec9795da02b9f
Florent Delgrange <florent.delgrange@gmail.com>
2020-04-21 11:15:37 +0200

    add math operations as functions to allow tensor operation functions

--- util/pdf/binary_concrete.py
+++ util/pdf/binary_concrete.py
@@ -3,46 +3,54 @@ from scipy import special as sp
 from util.pdf import logistic
 
 
-def density(temperature, alpha, x):
+def density(temperature, alpha, x, power=np.power, square=np.square):
     """
     Binary concrete probability density function
 
     :param temperature: temperature of the concrete density function (in (0, inf))
     :param alpha: location (in (0, inf))
-    :param x: binary concrete random variables (in (0, 1)). Bernoulli random variables when the
-              temperature converges to 0.
+    :param x: binary concrete random variables (in (0, 1)).
+              Bernoulli random variables when the temperature converges to 0.
+    :param power: power function
+    :param square: square function
     :return: the (binary concrete) probability of x given the temperature and location alpha
     """
-    return temperature * alpha * np.power(x, - temperature - 1) * np.power(1 - x, - temperature - 1) / \
-           np.square(alpha * np.power(x, (- temperature)) + np.power((1 - x), (- temperature)))
+    return temperature * alpha * power(x, - temperature - 1) * power(1 - x, - temperature - 1) / \
+           square(alpha * power(x, (- temperature)) + power((1 - x), (- temperature)))
 
 
-def log_density(temperature, log_alpha, x):
+def log_density(temperature, log_alpha, x, log=np.log, exp=np.exp, log1p=np.log1p):
     """
     Binary concrete log-probability
 
     :param temperature: temperature of the concrete density function (in (0, inf))
     :param log_alpha: log of the location of the distribution
     :param x: binary concrete random variables (in (0, 1))
+    :param log: log function
+    :param exp: exp function
+    :param log1p: log1p function
     :return: the (binary concrete) log-probability of x given the temperature and location alpha
     """
-    return np.log(temperature) + log_alpha - (temperature + 1) * np.log(x) + (temperature - 1) * np.log(1 - x) - \
-           2 * np.log1p(np.exp(log_alpha - temperature * (np.log(x) - np.log(1 - x))))
+    return log(temperature) + log_alpha - (temperature + 1) * log(x) + (temperature - 1) * log(1 - x) - \
+           2 * log1p(exp(log_alpha - temperature * (log(x) - log(1 - x))))
 
 
-def log_logistic_density(temperature, log_alpha, x):
+def log_logistic_density(temperature, log_alpha, x, log=np.log, exp=np.exp, log1p=np.log1p):
     """
     Binary concrete log-probability of a logistic random variable x (i.e., before applying the sigmoid)
 
     :param temperature: temperature of the concrete density function (in (0, inf))
     :param log_alpha: log of the location of the distribution
     :param x: binary concrete random variables (in (0, 1))
+    :param log: log function
+    :param exp: exp function
+    :param log1p: log1p function
     :return: the (binary concrete) log-probability of x given the temperature and location alpha
     """
-    return np.log(temperature) - temperature * x + log_alpha - 2 * np.log1p(np.exp(- temperature * x + log_alpha))
+    return log(temperature) - temperature * x + log_alpha - 2 * log1p(exp(- temperature * x + log_alpha))
 
 
-def log_diff(temperature, log_alpha_0, log_alpha_1, x):
+def log_diff(temperature, log_alpha_0, log_alpha_1, x, log=np.log, exp=np.exp, log1p=np.log1p):
     """
     Difference between two binary concrete log-probability with same temperature but different locations
 
@@ -50,21 +58,25 @@ def log_diff(temperature, log_alpha_0, log_alpha_1, x):
     :param log_alpha_0: log of the location of the first probability density function
     :param log_alpha_1: log of the location of the (subtracted) second density function
     :param x: binary concrete random variables (in (0, 1))
+    :param log: log function
+    :param exp: exp function
+    :param log1p: log1p function
     :return: compute log p_0(x) - log p_1(x) where p_i is a binary concrete density function with location a_i
     """
     return log_alpha_0 - log_alpha_1 + 2 * \
-           (np.log1p(np.exp(log_alpha_1 - temperature * (np.log(x) - np.log(1 - x)))) -
-            np.log1p(np.exp(log_alpha_0 - temperature * (np.log(x) - np.log(1 - x)))))
+           (log1p(exp(log_alpha_1 - temperature * (log(x) - log(1 - x)))) -
+            log1p(exp(log_alpha_0 - temperature * (log(x) - log(1 - x)))))
 
 
-def sample(temperature, log_alpha, logistic_noise):
+def sample(temperature, log_alpha, logistic_noise, sigmoid=sp.expit):
     """
     Sample a binary concrete random variable
 
     :param temperature: temperature of the binary concrete density function (in (0, inf))
     :param log_alpha: log of the location of the binary concrete density function
     :param logistic_noise (sampled from log(U) - log(1 - U), where U is a uniform random variable in (0, 1))
+    :param sigmoid: sigmoid function
     :return: a sampled binary concrete random variable from the corresponding density function with temperature and
              location alpha
     """
-    return sp.expit(logistic.sample(temperature, log_alpha, logistic_noise))
+    return sigmoid(logistic.sample(temperature, log_alpha, logistic_noise))
--- util/pdf/logistic.py
+++ util/pdf/logistic.py
@@ -1,31 +1,36 @@
 import numpy as np
 
 
-def density(temperature, mu, x):
+def density(temperature, mu, x, square=np.square, exp=np.exp):
     """
     Logistic probability density function
 
     :param temperature: temperature of the logistic function (in (0, inf))
     :param mu: location (in the binary logistic case, mu is log_alpha)
     :param x: logistic random variables (in (0, 1)).
+    :param square: square function
+    :param exp: exp function
     :return: the logistic probability of x given the temperature and location mu
     """
-    return temperature * np.exp(- temperature * x + mu) / np.square(1 + np.exp(- temperature * x + mu))
+    return temperature * exp(- temperature * x + mu) / square(1 + exp(- temperature * x + mu))
 
 
-def log_density(temperature, mu, x):
+def log_density(temperature, mu, x, log=np.log, exp=np.exp, log1p=np.log1p):
     """
     Log-Logistic probability density function
 
     :param temperature: temperature of the logistic function (in (0, inf))
     :param mu: location (in the binary logistic case, mu is log_alpha)
     :param x: logistic random variables (in (0, 1)).
+    :param log: log function
+    :param exp: exp function
+    :param log1p: log1p function
     :return: the log-logistic probability of x given the temperature and location mu
     """
-    return np.log(temperature) + (- temperature * x + mu) - 2 * np.log1p(np.exp(mu - temperature * x))
+    return log(temperature) + (- temperature * x + mu) - 2 * log1p(exp(mu - temperature * x))
 
 
-def log_diff(temperature, mu_0, mu_1, x):
+def log_diff(temperature, mu_0, mu_1, x, log=np.log, exp=np.exp, log1p=np.log1p):
     """
     Difference between two logistic log-probability with same temperature but different locations
 
@@ -33,9 +38,12 @@ def log_diff(temperature, mu_0, mu_1, x):
     :param mu_0: location of the first probability density function
     :param mu_1: location of the (subtracted) second density function
     :param x: logistic random variables (in (0, 1))
+    :param log: log function
+    :param exp: exp function
+    :param log1p: log1p function
     :return: compute log p_0(x) - log p_1(x) where p_i is a logistic density function with location mu_i
     """
-    return mu_0 - mu_1 + 2 * (np.log1p(np.exp(mu_1 - temperature * x)) - np.log1p(np.exp(mu_0 - temperature * x)))
+    return mu_0 - mu_1 + 2 * (log1p(exp(mu_1 - temperature * x)) - log1p(exp(mu_0 - temperature * x)))
 
 
 def sample(temperature, mu, logistic_noise):
