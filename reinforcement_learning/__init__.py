f15a8a9fcb1ffdebd15b76a5dfc5ab108a88fe70
florentdelgrange <florent.delgrange@gmail.com>
2020-08-13 14:41:38 +0200

    get rid of the take methods from numpy

--- reinforcement_learning/__init__.py
+++ reinforcement_learning/__init__.py
@@ -1,5 +1,5 @@
 labeling_functions = {
     'HumanoidBulletEnv-v0':
-        lambda states: states.take(0, axis=-1) + 0.8 <= 0.78,  # falling down
+        lambda states: states[..., 0] + 0.8 <= 0.78,  # falling down
         # np.count_nonzero(np.abs(observation[:, :, 8: 42][0::2]) > 0.99) > 0  # has stuck joints
 }
