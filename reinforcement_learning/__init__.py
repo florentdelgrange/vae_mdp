37bd031f79776baafa9c5eabb6aa9d157ec9a9bd
florentdelgrange <florent.delgrange@gmail.com>
2020-08-21 16:57:15 +0200

    BipedalWalker-v2 environment

--- reinforcement_learning/__init__.py
+++ reinforcement_learning/__init__.py
@@ -1,5 +1,10 @@
+import tensorflow as tf
+import math
+
 labeling_functions = {
     'HumanoidBulletEnv-v0':
         lambda states: states[..., 0] + 0.8 <= 0.78,  # falling down
-        # np.count_nonzero(np.abs(observation[:, :, 8: 42][0::2]) > 0.99) > 0  # has stuck joints
+    # np.count_nonzero(np.abs(observation[:, :, 8: 42][0::2]) > 0.99) > 0  # has stuck joints
+    'BipedalWalker-v2':
+        lambda states: tf.math.abs(states[..., 0]) > math.pi / 3.  # hull angle too high/low
 }
