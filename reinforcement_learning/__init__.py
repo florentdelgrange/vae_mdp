bfc1d97d815bced6d8f4e367d548fe1cdfbca692
florentdelgrange <florent.delgrange@gmail.com>
2021-03-03 16:11:51 +0100

    Pendulum v0 labeling function

--- reinforcement_learning/__init__.py
+++ reinforcement_learning/__init__.py
@@ -6,5 +6,12 @@ labeling_functions = {
         lambda observation: observation[..., 0] + 0.8 <= 0.78,  # falling down
     # np.count_nonzero(np.abs(observation[:, :, 8: 42][0::2]) > 0.99) > 0  # has stuck joints
     'BipedalWalker-v2':
-        lambda observation: tf.math.abs(observation[..., 0]) > math.pi / 3.  # hull angle too high/low
+        lambda observation: tf.math.abs(observation[..., 0]) > math.pi / 3.,  # hull angle too high/low
+    'Pendulum-v0':
+        lambda observation: tf.concat([
+            observation[..., 0] > 0. and tf.abs(observation[..., 1]) < tf.math.sin(math.pi / 2),  # easy: |theta| < 90
+            observation[..., 0] > 0. and tf.abs(observation[..., 1]) < tf.math.sin(math.pi / 6),  # soft: |theta| < 30
+            observation[..., 0] > 0. and tf.abs(observation[..., 1]) < tf.math.sin(math.pi / 9),  # hard: |theta| < 20
+        ], axis=-1)
+
 }
