967eec442518da04eb7bbc6af83bd39dd7a107e3
florentdelgrange <florent.delgrange@gmail.com>
2021-03-09 16:43:19 +0100

    DQN policy

--- reinforcement_learning/dqn_training.py
+++ reinforcement_learning/dqn_training.py
@@ -1,6 +1,7 @@
+import math
 import os
 import sys
-from typing import Tuple, Callable, Optional
+from typing import Tuple, Callable, Optional, List
 import threading
 import datetime
 from absl import app
@@ -11,18 +12,18 @@ import PIL
 import tensorflow as tf
 from tensorflow.python.keras.engine import sequential
 from tensorflow.python.keras.utils.generic_utils import Progbar
+from tf_agents.agents import CategoricalDqnAgent
 from tf_agents.agents.dqn import dqn_agent
 
 from tf_agents.drivers import dynamic_step_driver
 from tf_agents.drivers import dynamic_episode_driver
 from tf_agents.environments import tf_py_environment, parallel_py_environment
 from tf_agents.metrics import tf_metrics, tf_metric
-from tf_agents.networks import q_network
+from tf_agents.networks import q_network, categorical_q_network
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.utils import common
-from tf_agents.policies import policy_saver
+from tf_agents.policies import policy_saver, categorical_q_policy, boltzmann_policy, q_policy
 import tf_agents.trajectories.time_step as ts
-
 
 path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, path + '/../')
@@ -45,28 +46,41 @@ flags.DEFINE_float(
 flags.DEFINE_string(
     'save_dir', help='Save directory location', default='.'
 )
+flags.DEFINE_boolean(
+    'permissive_policy_saver',
+    help="Set this flag to save permissive policies of the current agent's policy, according to given temperatures.",
+    default=False
+)
+flags.DEFINE_multi_float(
+    'permissive_policy_temperature',
+    help='Temperature of the support of permissive policies produced.',
+    default=[0.5, 1., 1.5, 2., 2.5, 3., 3.5, 4., 4.5, 5.]
+)
 FLAGS = flags.FLAGS
 
 
 class DQNLearner:
-    def __init__(self,
-                 env_name: str,
-                 env_suite,
-                 num_iterations: int = int(2e5),
-                 initial_collect_steps: int = int(1e4),
-                 collect_steps_per_iteration: int = 1,
-                 replay_buffer_capacity: int = int(1e6),
-                 network_fc_layer_params: Tuple[int, ...] = (100, 50),
-                 gamma: float = 0.99,
-                 learning_rate: float = 1e-3,
-                 log_interval: int = 2500,
-                 num_eval_episodes: int = 30,
-                 eval_interval: int = int(1e4),
-                 parallelization: bool = True,
-                 num_parallel_environments: int = 4,
-                 batch_size: int = 64,
-                 debug: bool = False,
-                 save_directory_location: str = '.'):
+    def __init__(
+            self,
+            env_name: str,
+            env_suite,
+            num_iterations: int = int(2e5),
+            initial_collect_steps: int = int(1e4),
+            collect_steps_per_iteration: int = 1,
+            replay_buffer_capacity: int = int(1e6),
+            network_fc_layer_params: Tuple[int, ...] = (100, 50),
+            gamma: float = 0.99,
+            learning_rate: float = 1e-3,
+            log_interval: int = 2500,
+            num_eval_episodes: int = 30,
+            eval_interval: int = int(1e4),
+            parallelization: bool = True,
+            num_parallel_environments: int = 4,
+            batch_size: int = 64,
+            debug: bool = False,
+            save_directory_location: str = '.',
+            permissive_policy_temperatures: Optional[List[float]] = None
+    ):
 
         if collect_steps_per_iteration is None:
             collect_steps_per_iteration = batch_size
@@ -93,6 +107,7 @@ class DQNLearner:
         self.num_parallel_environments = num_parallel_environments
 
         self.batch_size = batch_size
+        self.permissive_policy_temperatures = permissive_policy_temperatures
 
         if parallelization:
             self.tf_env = tf_py_environment.TFPyEnvironment(parallel_py_environment.ParallelPyEnvironment(
@@ -116,28 +131,7 @@ class DQNLearner:
         self.observation_spec = self.tf_env.observation_spec()
         self.action_spec = self.tf_env.action_spec()
 
-        num_actions = self.action_spec.maximum - self.action_spec.minimum + 1
-
-        # Define a helper function to create Dense layers configured with the right
-        # activation and kernel initializer.
-        def dense_layer(num_units):
-            return tf.keras.layers.Dense(
-                num_units,
-                activation=tf.keras.activations.relu,
-                kernel_initializer=tf.keras.initializers.VarianceScaling(
-                    scale=2.0, mode='fan_in', distribution='truncated_normal'))
-
-        # QNetwork consists of a sequence of Dense layers followed by a dense layer
-        # with `num_actions` units to generate one q_value per available action as
-        # it's output.
-        dense_layers = [dense_layer(num_units) for num_units in network_fc_layer_params]
-        q_values_layer = tf.keras.layers.Dense(
-            num_actions,
-            activation=None,
-            kernel_initializer=tf.keras.initializers.RandomUniform(
-                minval=-0.03, maxval=0.03),
-            bias_initializer=tf.keras.initializers.Constant(-0.2))
-        q_net = q_network.QNetwork(
+        self.q_network = q_network.QNetwork(
             self.tf_env.observation_spec(), self.tf_env.action_spec(), fc_layer_params=network_fc_layer_params)
 
         optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
@@ -147,10 +141,12 @@ class DQNLearner:
         self.tf_agent = dqn_agent.DqnAgent(
             self.tf_env.time_step_spec(),
             self.tf_env.action_spec(),
-            q_network=q_net,
+            q_network=self.q_network,
             optimizer=optimizer,
             td_errors_loss_fn=common.element_wise_squared_loss,
-            train_step_counter=self.global_step)
+            train_step_counter=self.global_step,
+            # emit_log_probability=True
+        )
 
         self.tf_agent.initialize()
 
@@ -170,7 +166,7 @@ class DQNLearner:
             num_steps=2).prefetch(3)
         self.iterator = iter(self.dataset)
 
-        self.checkpoint_dir = os.path.join(save_directory_location, 'saves', env_name, 'sac_training_checkpoint')
+        self.checkpoint_dir = os.path.join(save_directory_location, 'saves', env_name, 'dql_training_checkpoint')
         self.train_checkpointer = common.Checkpointer(
             ckpt_dir=self.checkpoint_dir,
             max_to_keep=1,
@@ -179,8 +175,8 @@ class DQNLearner:
             replay_buffer=self.replay_buffer,
             global_step=self.global_step
         )
-        self.stochastic_policy_dir = os.path.join(save_directory_location, 'saves', env_name, 'policy')
-        self.stochastic_policy_saver = policy_saver.PolicySaver(self.tf_agent.policy)
+        self.policy_dir = os.path.join(save_directory_location, 'saves', env_name, 'policy')
+        self.policy_saver = policy_saver.PolicySaver(self.tf_agent.policy)
 
         self.num_episodes = tf_metrics.NumberOfEpisodes()
         self.env_steps = tf_metrics.EnvironmentSteps()
@@ -196,9 +192,33 @@ class DQNLearner:
 
         current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         train_log_dir = os.path.join(
-            save_directory_location, 'logs', 'gradient_tape', env_name, 'sac_agent_training', current_time)
+            save_directory_location, 'logs', 'gradient_tape', env_name, 'dql_agent_training', current_time)
         self.train_summary_writer = tf.summary.create_file_writer(train_log_dir)
         self.save_directory_location = os.path.join(save_directory_location, 'saves', env_name)
+
+        if os.path.exists(self.checkpoint_dir):
+            self.train_checkpointer.initialize_or_restore()
+            self.global_step = tf.compat.v1.train.get_global_step()
+            print("Checkpoint loaded! global_step={}".format(self.global_step.numpy()))
+        if not os.path.exists(self.policy_dir):
+            os.makedirs(self.policy_dir)
+
+    def permissive_policy(self, temperature):
+        permissive_policy_dir = os.path.join(
+            self.save_directory_location,
+            'policy',
+            'permissive_policy_temperature={:g}'.format(temperature)
+        )
+
+        policy = q_policy.QPolicy(
+            self.tf_env.time_step_spec(),
+            self.tf_env.action_spec(),
+            q_network=self.q_network,
+            emit_log_probability=True,
+        )
+        policy = boltzmann_policy.BoltzmannPolicy(policy, temperature=temperature)
+        stochastic_policy_saver = policy_saver.PolicySaver(policy)
+        stochastic_policy_saver.save(permissive_policy_dir)
 
     def train_and_eval(self, display_progressbar: bool = True, display_interval: float = 0.1):
 
@@ -215,24 +235,13 @@ class DQNLearner:
         if not self.parallelization:
             metrics += ['num_episodes', 'env_steps']
 
-        class AbstractLoss:
-            def __init__(self):
-                self.loss = 0.
-
-        train_loss = AbstractLoss()
-
+        train_loss = 0.
         # load the checkpoint
-        if os.path.exists(self.checkpoint_dir):
-            self.train_checkpointer.initialize_or_restore()
-            self.global_step = tf.compat.v1.train.get_global_step()
-            print("Checkpoint loaded! global_step={}".format(self.global_step.numpy()))
-        if not os.path.exists(self.stochastic_policy_dir):
-            os.makedirs(self.stochastic_policy_dir)
 
         def update_progress_bar(num_steps=1):
             if display_progressbar:
                 log_values = [
-                    ('loss', train_loss.loss),
+                    ('loss', train_loss),
                     ('replay_buffer_frames', self.replay_buffer.num_frames()),
                     ('training_avg_returns', self.avg_return.result()),
                 ]
@@ -263,7 +272,7 @@ class DQNLearner:
             # Use data from the buffer and update the agent's network.
             # experience = replay_buffer.gather_all()
             experience, _ = next(self.iterator)
-            train_loss = self.tf_agent.train(experience)
+            train_loss = self.tf_agent.train(experience).loss
 
             step = self.tf_agent.train_step_counter.numpy()
 
@@ -271,9 +280,12 @@ class DQNLearner:
 
             if step % self.log_interval == 0:
                 self.train_checkpointer.save(self.global_step)
-                self.stochastic_policy_saver.save(self.stochastic_policy_dir)
+                self.policy_saver.save(self.policy_dir)
+                if self.permissive_policy_temperatures is not None:
+                    for temperature in self.permissive_policy_temperatures:
+                        self.permissive_policy(temperature=temperature)
                 with self.train_summary_writer.as_default():
-                    tf.summary.scalar('loss', train_loss.loss, step=step)
+                    tf.summary.scalar('loss', train_loss, step=step)
                     tf.summary.scalar('training average returns', self.avg_return.result(), step=step)
 
             if step % self.eval_interval == 0:
@@ -283,7 +295,7 @@ class DQNLearner:
     def eval(self, step: int = 0, progressbar: Optional = None):
         avg_eval_return = tf_metrics.AverageReturnMetric()
         avg_eval_episode_length = tf_metrics.AverageEpisodeLengthMetric()
-        saved_policy = tf.compat.v2.saved_model.load(self.stochastic_policy_dir)
+        saved_policy = tf.compat.v2.saved_model.load(self.policy_dir)
         self.eval_env.reset()
         dynamic_episode_driver.DynamicEpisodeDriver(
             self.eval_env,
@@ -324,7 +336,12 @@ def main(argv):
         num_iterations=params['steps'],
         num_parallel_environments=params['num_parallel_env'],
         save_directory_location=params['save_dir'],
+        permissive_policy_temperatures=params['permissive_policy_temperature']
     )
+    if params['permissive_policy_saver']:
+        for temperature in params['permissive_policy_temperature']:
+            learner.permissive_policy(temperature)
+        return 0
     learner.train_and_eval()
     return 0
 
