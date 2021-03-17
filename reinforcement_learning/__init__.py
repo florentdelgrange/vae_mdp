ac36f34d30ac2ddbcebe0a53b05e5296ed009842
theGreatGiorgio <florent.delgrange@gmail.com>
2021-03-17 16:37:00 +0100

    LunarLanderContinuous

--- reinforcement_learning/__init__.py
+++ reinforcement_learning/__init__.py
@@ -20,5 +20,23 @@ labeling_functions = {
         lambda observation: tf.stack([
             tf.abs(observation[..., 0]) < 1.5,  # cart position is less than 1.5
             tf.abs(observation[..., 2]) < 0.15,  # pole angle is inferior than 9 degrees
-        ], axis=-1)
+        ], axis=-1),
+    'LunarLander-v2':
+        lambda observation: tf.stack([
+            tf.abs(observation[..., 0]) <= 0.15,  # land along the lunar pad x-position
+            tf.abs(observation[..., 0]) >= 0.8,  # close to the edge of the frame
+            # close to the lunar pad
+            tf.math.logical_and(tf.abs(observation[..., 1]) <= 0.3, tf.abs(observation[..., 0]) <= 0.3),
+            tf.abs(observation[..., 1]) <= 0.02,  # land along the lunar pad y-position
+            observation[..., 2] == 0.,  # horizontal speed is 0
+            observation[..., 3] == 0.,   # vertical speed is 0
+            observation[..., 3] <= -0.5,  # fast vertical (landing) speed
+            tf.abs(observation[..., 4]) <= math.pi / 3,  # lander angle is safe
+            tf.abs(observation[..., 4]) <= math.pi / 6,  # weak lander angle
+            observation[..., 5] == 0.,  # angular velocity is 0
+            tf.cast(observation[..., 6], dtype=tf.bool),  # left leg ground contact
+            tf.cast(observation[..., 7], dtype=tf.bool)   # right leg ground contact
+        ], axis=-1),
 }
+
+labeling_functions['LunarLanderContinuous-v2'] = labeling_functions['LunarLander-v2']
