5d06c27850f4ceadaef7ae840d9823e79915e513
theGreatGiorgio <florent.delgrange@gmail.com>
2021-04-07 13:46:37 +0200

    new HumanoidBulletEnv labels

--- reinforcement_learning/__init__.py
+++ reinforcement_learning/__init__.py
@@ -2,9 +2,16 @@ import tensorflow as tf
 import math
 
 labeling_functions = {
-    'HumanoidBulletEnv-v0':
-        lambda observation: observation[..., 0] + 0.8 <= 0.78,  # falling down
-    # np.count_nonzero(np.abs(observation[:, :, 8: 42][0::2]) > 0.99) > 0  # has stuck joints
+    'HumanoidBulletEnv-v0': lambda observation: tf.stack([
+            # falling down -- observation[0] is the head position, 0.8 is the initial position
+            observation[..., 0] + 0.8 <= 0.78,
+            # has stuck joints
+            # tf.expand_dims(tf.math.count_nonzero(tf.abs(observation[..., 8: 42][0::2]) > 0.99) > 0, axis=-1),
+            # feet contact
+            tf.cast(observation[..., -2], tf.bool), tf.cast(observation[..., -1], tf.bool),
+            # move forward
+            observation[..., 3] > 0.,
+        ], axis=-1),
     'BipedalWalker-v2':
         lambda observation: tf.stack([
             tf.math.abs(observation[..., 0]) > math.pi / 3.,  # hull angle too high/low (unsafe flag)
@@ -13,12 +20,12 @@ labeling_functions = {
         ], axis=-1),
     'Pendulum-v0':  # safe labels
         lambda observation: tf.stack([
-            tf.logical_and(observation[..., 0] > 0., tf.abs(observation[..., 1]) < tf.math.sin(math.pi / 2)),
             # easy: |theta| < 90
-            tf.logical_and(observation[..., 0] > 0., tf.abs(observation[..., 1]) < tf.math.sin(math.pi / 6)),
+            tf.logical_and(observation[..., 0] > 0., tf.abs(observation[..., 1]) < tf.math.sin(math.pi / 2)),
             # soft: |theta| < 30
-            tf.logical_and(observation[..., 0] > 0., tf.abs(observation[..., 1]) < tf.math.sin(math.pi / 9)),
+            tf.logical_and(observation[..., 0] > 0., tf.abs(observation[..., 1]) < tf.math.sin(math.pi / 6)),
             # hard: |theta| < 20
+            tf.logical_and(observation[..., 0] > 0., tf.abs(observation[..., 1]) < tf.math.sin(math.pi / 9)),
         ], axis=-1),
     'CartPole-v0':  # safe labels
         lambda observation: tf.stack([
