4c42a3cb2d1a3b258f12a753d8d7ebc171efe89d
theGreatGiorgio <florent.delgrange@gmail.com>
2021-04-09 18:21:28 +0200

    probability function frequency estimator

--- verification/local_losses.py
+++ verification/local_losses.py
@@ -1,4 +1,4 @@
-from collections import Callable
+from collections import Callable, namedtuple
 from typing import Optional
 
 import tensorflow as tf
@@ -18,30 +18,51 @@ from verification.latent_environment import LatentPolicyOverRealStateSpace, Disc
 tfd = tfp.distributions
 
 
-def frequency_estimation_probability_function(
-        latent_state: tf.Tensor,
-        latent_action: tf.Tensor,
-        next_latent_state: tf.Tensor,  # Optional[tf.Tensor] = None,
-        # next_latent_state_no_label: Optional[tf.Tensor] = None,
-        # next_label: Optional[tf.Tensor] = None,
-        # label_size: Optional[int] = None,
-):
-    #  if (next_latent_state is None) == (next_latent_state_no_label is None):
-    #      raise ValueError('Must either pass next latent states or next latent states without labels')
-    #  if next_latent_state_no_label is not None:
-    #      assert next_label is not None
-    #  else:
-    #      assert label_size is not None
+class FrequencyEstimationTransitionFunction:
+    def __init__(self, latent_states: tf.Tensor, latent_actions: tf.Tensor, next_latent_states: tf.Tensor):
+        self.num_states = tf.shape(latent_states)[1]  # first axis is batch, second is latent state size
+        self.num_actions = tf.shape(latent_actions)[1]  # first axis is batch, second is a one-hot vector
+        states = tf.reduce_sum(latent_states * 2 ** tf.range(self.num_states), axis=-1)
+        actions = tf.argmax(latent_actions, axis=-1)
+        next_states = tf.reduce_sum(next_latent_states * 2 ** tf.range(self.num_states), axis=-1)
 
-    #  if next_latent_state_no_label is None:
-    #  next_label,  = next_latent_state[..., :label_size]
-    #  next_latent_state_no_label = next_latent_state[...: label_size:]
+        # flat transition indices
+        transitions = states * self.num_actions * self.num_states + actions * self.num_states + next_states
+        transitions, _, count = tf.unique_with_counts(transitions)
+        transitions = tf.stack([transitions // (self.num_states * self.num_actions),  # state index
+                                (transitions // self.num_states) % self.num_actions,  # action index
+                                transitions % self.num_states],  # next state index
+                               axis=-1)
+        transition_counter = tf.sparse.SparseTensor(
+            indices=transitions, values=count, dense_shape=(self.num_states, self.num_actions, self.num_states))
+        state_action_pair_counter = tf.sparse.reduce_sum(transition_counter, axis=-1, output_is_sparse=True)
+        probs = tf.reduce_sum(
+            tf.map_fn(
+                fn=lambda x: tf.where(
+                    condition=tf.reduce_all(transition_counter.indices[..., :-1] == x[0], axis=-1),
+                    x=transition_counter.values / x[1],
+                    y=tf.zeros(tf.shape(transition_counter.values))),
+                elems=(state_action_pair_counter.indices, state_action_pair_counter.values)),
+            axis=0)
 
-    latent_state_size = tf.shape(latent_state)[1]  # first axis is batch, second is latent state size
-    number_of_discrete_actions = tf.shape(latent_action)[1]  # first axis is batch, second is a one-hot vector
-    latent_state_indices = tf.reduce_sum(latent_state * 2 ** tf.range(latent_state_size), axis=-1)
-    latent_action_indices = tf.argmax(latent_action, axis=-1)
-    next_latent_state_indices = tf.reduce_sum(latent_state * 2 ** tf.range(latent_state_size), axis=-1)
+        self.transition_tensor = tf.sparse.SparseTensor(
+            indices=transitions, values=probs, dense_shape=(self.num_states, self.num_actions, self.num_states))
+
+    def __call__(self, latent_state: tf.Tensor, latent_action: tf.Tensor, *next_latent_state):
+        state = tf.reduce_sum(latent_state * 2 ** tf.range(self.num_states), axis=-1)
+        action = tf.argmax(latent_action, axis=-1)
+
+        def _get_prob_value(*args):
+            state, action, next_state = args
+            probs = tf.squeeze(tf.sparse.slice(self.transition_tensor, [state, action, next_state], [1, 1, 1]))
+            return 0. if tf.equal(tf.size(probs), 0) else probs
+
+        def _prob(*value):
+            next_latent_state = tf.concat(value, axis=-1)
+            next_state = tf.reduce_sum(next_latent_state * 2 ** tf.range(self.num_states), axis=-1)
+            return tf.map_fn(fn=_get_prob_value, elems=(state, action, next_state))
+
+        return namedtuple('next_state_transition_distribution', ['prob'])(_prob)
 
 
 def compute_local_losses_from_samples(
