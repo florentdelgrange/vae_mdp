e4e65a0409d622a120a5dac2eafa4f80f340fd80
florentdelgrange <florent.delgrange@gmail.com>
2021-07-29 17:13:12 +0200

    up

--- reinforcement_learning/environments/acrobot.py
+++ reinforcement_learning/environments/acrobot.py
@@ -6,6 +6,6 @@ class AcrobotEnvRandomInit(AcrobotEnv):
     def reset(self):
         high = np.array([np.pi, np.pi, self.MAX_VEL_1, self.MAX_VEL_2])
         loc = np.zeros(shape=(4,))
-        scale = np.array([np.pi / 6, np.pi / 6, self.MAX_VEL_1 / 14, self.MAX_VEL_2 / 6])
+        scale = np.array([np.pi / 9, np.pi / 9, np.pi / 6, np.pi / 4])
         self.state = np.clip(np.random.normal(loc, scale), -high, high)
         return self._get_ob()
