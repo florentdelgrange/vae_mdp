8032dc38bc34fcb929445b06fba10701adf35ea6
florentdelgrange <florent.delgrange@gmail.com>
2021-08-27 15:42:47 +0200

    replaced tfp by numpy

--- reinforcement_learning/environments/PerturbedEnvironment.py
+++ reinforcement_learning/environments/PerturbedEnvironment.py
@@ -3,8 +3,7 @@ from typing import Any, Optional
 from tf_agents.environments.wrappers import PyEnvironmentBaseWrapper
 from tf_agents.typing import types
 from tf_agents.trajectories import time_step as ts
-import tensorflow_probability as tfp
-tfd = tfp.distributions
+import numpy as np
 
 
 class PerturbedEnvironment(PyEnvironmentBaseWrapper):
@@ -16,15 +15,18 @@ class PerturbedEnvironment(PyEnvironmentBaseWrapper):
 
     def step(self, action: types.NestedArray) -> ts.TimeStep:
         if self.action_noise > 0:
-            _action = tfd.MultivariateNormalDiag(loc=action, scale_identity_multiplier=self.action_noise).sample()
+            _action = np.random.multivariate_normal(
+                mean=action,
+                cov=np.diag(self.action_noise ** 2 * np.ones(shape=np.shape(action))))
         else:
             _action = action
 
         time_step = self.wrapped_env().step(_action)
 
         if self.state_noise > 0:
-            _observation = tfd.MultivariateNormalDiag(
-                loc=time_step.observation, scale_identity_multiplier=self.state_noise).sample()
+            _observation = np.random.multivariate_normal(
+                mean=time_step.observation,
+                cov=np.diag(self.state_noise ** 2 * np.ones(shape=np.shape(time_step.observation))))
         else:
             _observation = time_step.observation
 
